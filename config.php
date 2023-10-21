0e1872efa45517da0b763ea15b23df805a8c8f91
Rahman Hakim <rhmnhkm13@gmail.com>
2023-10-21 11:23:47 +0700

    commit pertama

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+//membuat variabel, ubah sesuai nama host dan database pada hosting
+$host ="localhost";
+$user ="root";
+$pass ="root";
+$db ="guest_book";
+
+$mysqli = new mysqli($host, $user, $pass, $db);
+
+?>
